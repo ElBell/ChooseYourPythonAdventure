07cdd43de3fc8b0cff16716c6cb2dc656fcbd497
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-01 20:19:19 -0400

    fixed signup

--- Player/views.py
+++ Player/views.py
@@ -27,7 +27,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('player_home')
     else:
         form = SignUpForm()
     return render(request, 'Player/signup.html', {'form': form})
