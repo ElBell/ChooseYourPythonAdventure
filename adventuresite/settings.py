dc8a3d6bb32eb2e53371451d841df31fad3771c0
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-03 22:42:41 -0400

    Put Kenneth's stuff back

--- adventuresite/settings.py
+++ adventuresite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +85,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
