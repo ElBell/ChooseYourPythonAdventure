f22764473e28ab6cc08cb99797d807db0ebc6296
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-03 22:52:08 -0400

    changed DATABASES.update

--- adventuresite/settings.py
+++ adventuresite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+
+import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -83,7 +85,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DATABASES['default'] = dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
